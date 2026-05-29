12c64a797b22f1cee6e1cdae3d7ce4f0b2236096
Andreas Salminger <andisalminger@hotmail.com>
2026-05-29 13:14:24 +0200

    added TODOs for further investigation

--- E2ETests.ApproachA.SeedInBody/Tests/EmployeeTests.cs
+++ E2ETests.ApproachA.SeedInBody/Tests/EmployeeTests.cs
@@ -55,3 +55,22 @@ public class EmployeeTests : PlaywrightTestBase
         await Expect(Page.Locator(".employee-name")).ToHaveValueAsync("Updated Name");
     }
 }
+
+/*
+TODO:
+
+1. Customization wäre schon cool --> evtl. Dictionary in Attribute mitgeben (PropName -> Value)
+
+    BUilder mitgeben in Seed Attribut
+	    IE2ETestDataBuilderInterface etc. -> Build() Methode
+	    diese Methode wird in Seed aufgerufen
+	
+	    Evtl. über ActionFilterAttribute custom Values mitgeben (zB. Anderer Employee Name)?? Wsl. nicht möglich wegen Mehrfachvererbung
+	
+    -> zB. DepartmentBUilder + EmployeeWithDepartmentBuilder Klassen
+
+    Vorteil: Ich muss nicht jedes mal das Attribut schreiben
+
+2. Respawn Libary anschauen für snapshot/resets der DB
+3. Raw-SQL mit Sql-Server für DB Snapshots
+ */
