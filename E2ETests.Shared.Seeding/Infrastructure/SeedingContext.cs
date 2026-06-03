6c0b68054f30692a494dc10dd16ed0219a0d19c5
Andreas Salminger <andisalminger@hotmail.com>
2026-06-03 11:00:28 +0200

    added TODO

--- E2ETests.Shared.Seeding/Infrastructure/SeedingContext.cs
+++ E2ETests.Shared.Seeding/Infrastructure/SeedingContext.cs
@@ -1,4 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using System.Reflection;
 using WebApp.Data;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace E2ETests.Shared.Seeding.Infrastructure;
 
@@ -20,6 +24,13 @@ public sealed class SeedingContext : IAsyncDisposable
             _db.Set<T>().Remove(entity);
             await _db.SaveChangesAsync();
         });
+
+        // TODO: This kind of cleanup is fragile because:
+        // - The employee may already be deleted. (for example in a test that tests deletion)
+        // - The entity may be tracked in an old state. (for example in a test that tests updates)
+        // - Foreign key dependencies may still exist.
+        // - A test might partially modify or move the data.
+        // - EF Core may throw concurrency exceptions for expected - but - missing rows.
 
         return entity;
     }
